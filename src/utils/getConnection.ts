4f346ade1aefcee922e51d5ce8c54436f06f8f48
电子腋毛程序/Electronic hirci program <luozeting97@gmail.com>
2022-11-12 11:40:07 +1300

    Switch to new pocketbase domain

--- src/utils/getConnection.ts
+++ src/utils/getConnection.ts
@@ -1,6 +1,6 @@
 import PocketBase from "pocketbase";
 const getConnection = async () => {
-  const client = new PocketBase("https://connection.yemaoren.tk/");
+  const client = new PocketBase("https://connection.yemaoren.net/");
 
   return client;
 };
